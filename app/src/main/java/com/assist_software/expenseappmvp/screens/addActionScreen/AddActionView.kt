df57ca2ebad62420650d532f0cc95cc8cac027bf
Razvan <prundel.razvan@gmail.com>
2020-09-14 11:10:02 +0300

    Date + time picker + save user input to DB

--- app/src/main/java/com/assist_software/expenseappmvp/screens/addActionScreen/AddActionView.kt
+++ app/src/main/java/com/assist_software/expenseappmvp/screens/addActionScreen/AddActionView.kt
@@ -1,13 +1,18 @@
 package com.assist_software.expenseappmvp.screens.addActionScreen
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.view.View
+import android.widget.DatePicker
 import android.widget.ImageView
+import android.widget.TimePicker
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.GridLayoutManager
 import com.assist_software.expenseappmvp.R
@@ -25,18 +30,90 @@ import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_add_action.view.*
 import kotlinx.android.synthetic.main.transaction_toolbar.view.*
 import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
 import java.util.*
 
-class AddActionView(var activity: AddActionActivity) {
+class AddActionView(var activity: AddActionActivity) : DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
     val layout: View = View.inflate(activity, R.layout.activity_add_action, null)
-    private val c = Calendar.getInstance()
+    private var day = 0
+    private var month = 0
+    private var year = 0
+    private var hour = 0
+    private var minute = 0
+
+    private var savedDay = 0
+    private var savedMonth = 0
+    private var savedYear = 0
+    private var savedHour = 0
+    private var savedMinute = 0
 
     private var defaultDetailsText: String = ""
     private var defaultDetailsImage: ByteArray = ByteArray(0)
 
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay = dayOfMonth
+        savedMonth = month + 1
+        savedYear = year
+
+        getDateTimeCalendar()
+
+        TimePickerDialog(activity, this, hour, minute, true).show()
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+
+        layout.date_EditText.setText("$savedYear-$savedMonth-$savedDay at $savedHour:$savedMinute")
+    }
+
     fun showHomeScreen() {
         HomeActivity.start(activity)
         activity.finish()
+    }
+
+    fun initToolbar() {
+        val toolbar = layout.toolbar as Toolbar
+        activity.run {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowHomeEnabled(true)
+        }
+        toolbar.run {
+            title = activity.getString(R.string.add_action)
+            setTitleTextAppearance(activity, R.style.HomeToolbarFont)
+        }
+    }
+
+    private fun getDateTimeCalendar(){
+        val cal = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getUTCTimestamp(): Long {
+        val sdf = SimpleDateFormat("dd-MM-yyyy hh:mm")
+        val dateString = "$savedDay-$savedMonth-$savedYear $savedHour:$savedMinute"
+        val date = sdf.parse(dateString)
+        var calendar = Calendar.getInstance()
+        calendar.time = date
+        return calendar.timeInMillis
+    }
+
+    fun initDatePicker() {
+        getDateTimeCalendar()
+        val datePickerDialog = DatePickerDialog(activity, this, year, month, day)
+        datePickerDialog.show()
+        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
     }
 
     private fun loadImageInViewFromLocalStorage(data: Intent?) {
@@ -51,20 +128,6 @@ class AddActionView(var activity: AddActionActivity) {
         layout.details_ImageView.run {
             setBackgroundResource(android.R.color.transparent)
             setImageBitmap(bitmap)
-        }
-    }
-
-    fun initToolbar() {
-        val toolbar = layout.toolbar as Toolbar
-        activity.run {
-            setSupportActionBar(toolbar)
-            supportActionBar?.setDisplayShowTitleEnabled(false)
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-            supportActionBar?.setDisplayShowHomeEnabled(true)
-        }
-        toolbar.run {
-            title = activity.getString(R.string.add_action)
-            setTitleTextAppearance(activity, R.style.HomeToolbarFont)
         }
     }
 
@@ -124,45 +187,23 @@ class AddActionView(var activity: AddActionActivity) {
         return RxView.clicks(layout.button_save)
     }
 
-    private fun getUTCTimestamp(): Long {
-        return c.timeInMillis
-    }
-
-    fun initDatePicker() {
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
-        val hour = c.get(Calendar.HOUR_OF_DAY)
-        val min = c.get(Calendar.MINUTE)
-
-        val dpd = DatePickerDialog(
-                activity,
-                { view, mYear, mMonth, mDay ->
-                    layout.date_EditText.setText(mYear.toString() + "-" + mMonth.toString() + "-" + mDay.toString() + " at " + hour + ":" + min)
-                },
-                year,
-                month,
-                day
-        )
-        dpd.show()
-        dpd.datePicker.maxDate = System.currentTimeMillis()
-    }
-
     fun getIncome(uid: String, category: String): Income {
         if (layout.amount_EditText.text.toString() != null) {
             defaultDetailsText = layout.details_EditText.text.toString()
         }
-        if(layout.details_ImageView != null){
+        if (layout.details_ImageView != null) {
             defaultDetailsImage = imageToBitmap(layout.details_ImageView)
         }
-        val amount = if (layout.amount_EditText.text.toString().isEmpty()) 0.0 else layout.amount_EditText.text.toString().toDouble()
+        val amount = if (layout.amount_EditText.text.toString()
+                .isEmpty()
+        ) 0.0 else layout.amount_EditText.text.toString().toDouble()
         return Income(
-                uid = uid,
-                incomeDate = getUTCTimestamp(),
-                incomeAmount = amount,
-                incomeCategory = category,
-                incomeDetails = defaultDetailsText,
-                incomeImage = defaultDetailsImage
+            uid = uid,
+            incomeDate = getUTCTimestamp(),
+            incomeAmount = amount,
+            incomeCategory = category,
+            incomeDetails = defaultDetailsText,
+            incomeImage = defaultDetailsImage
         )
     }
 
@@ -170,17 +211,19 @@ class AddActionView(var activity: AddActionActivity) {
         if (layout.amount_EditText.text.toString().isNotEmpty()) {
             defaultDetailsText = layout.details_EditText.text.toString()
         }
-        if(layout.details_ImageView != null){
+        if (layout.details_ImageView != null) {
             defaultDetailsImage = imageToBitmap(layout.details_ImageView)
         }
-        val amount = if (layout.amount_EditText.text.toString().isEmpty()) 0.0 else layout.amount_EditText.text.toString().toDouble()
+        val amount = if (layout.amount_EditText.text.toString()
+                .isEmpty()
+        ) 0.0 else layout.amount_EditText.text.toString().toDouble()
         return Expense(
-                uid = uid,
-                expenseDate = getUTCTimestamp(),
-                expenseAmount = amount,
-                expenseCategory = category,
-                expenseDetails = defaultDetailsText,
-                expenseImage = defaultDetailsImage
+            uid = uid,
+            expenseDate = getUTCTimestamp(),
+            expenseAmount = amount,
+            expenseCategory = category,
+            expenseDetails = defaultDetailsText,
+            expenseImage = defaultDetailsImage
         )
     }
 
@@ -195,7 +238,7 @@ class AddActionView(var activity: AddActionActivity) {
         return returnArray
     }
 
-    fun changeTitle(){
+    fun changeTitle() {
         val toolbar = layout.toolbar as Toolbar
         toolbar.title = layout.context.getString(R.string.edit_action)
     }
